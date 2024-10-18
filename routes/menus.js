71f8e84c7cf2ea96ac7c80232a906f06e3cda28a
sdh02142 <sdh02142@naver.com>
2024-10-18 16:09:29 +0900

    first commit

--- routes/menus.js
+++ routes/menus.js
@@ -1,36 +1,63 @@
 import express from 'express';
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({
+  log: ["query", "info", "warn", "error"],
+  errorFormat: "pretty",
+});
+
 const router = express.Router();
 
-router.get('/stats', (req, res, next) => {
+router.get('/stats', async (req, res, next) => {
+    const total = await prisma.orderHistory.groupBy({
+        by: ['menu_id'],
+        _count: {
+            menu_id: true,
+        },
+    });
+
+    var toMe = 0;
+    for(let i = 0; i < total.length; i++){
+        toMe = toMe + total[i]['_count']['menu_id'];
+    };
+    const toOr = total.length;
+
+    console.log(toMe);
+    console.log(toOr);
+
     res.status(200).json({
         stats: {
-            totalMenus: 3,
-            totalOrders: 10,
+            totalMenus: toMe,
+            totalOrders: toOr,
             totalSales: 30000
         }
     });
 });
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
+    const orderList = await prisma.menu.findMany({
+    });
+
     res.status(200).json({
-        menus: [
-            {
-                id: 1,
-                name: 'Latte',
-                type: 'Coffee',
-                temperature: 'hot',
-                price: 4500,
-                totalOrders: 5
-            },
-            {
-                id: 2,
-                name: 'Iced Tea',
-                type: 'Tea',
-                temperature: 'ice',
-                price: 3000,
-                totalOrders: 10
-            }
-        ]
+        menus: orderList
+        // [
+        //     {
+        //         id: 1,
+        //         name: 'Latte',
+        //         type: 'Coffee',
+        //         temperature: 'hot',
+        //         price: 4500,
+        //         totalOrders: 5
+        //     },
+        //     {
+        //         id: 2,
+        //         name: 'Iced Tea',
+        //         type: 'Tea',
+        //         temperature: 'ice',
+        //         price: 3000,
+        //         totalOrders: 10
+        //     }
+        // ]
     });
 });
 
